8827577e6d6920e394e7d4b8a2e0fa9a00e6ab3c
sleaz0id <mat.staszczyk@gmail.com>
2016-03-04 12:12:14 +0100

    my_each test added

--- enums/spec/enums_spec.rb
+++ enums/spec/enums_spec.rb
@@ -6,5 +6,13 @@ describe 'enums' do
     @result = Array.new
   end
 
+  describe 'my_each method' do 
+    it 'squares each element of array' do
+      @test_array.my_each { |i| @result << i**2 }
+      expect(@result).to eq ([1,81,4,25,49,9,25])
+    end 
+  end
+
+
 
 end
